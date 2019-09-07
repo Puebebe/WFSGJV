89d45eed1ed72e90eea6f8bfb330cf395d17b7ec
Dirtowladny <dirtowladny@gmail.com>
2019-09-07 06:29:44 +0200

    HPDisplay

---
+++ Assets/Scripts/HPDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPDisplay : MonoBehaviour
+{
+	public RectTransform HPBar;
+	public Image Vignette;
+
+	public void DisplayHP(float hp)
+	{
+		if (HPBar != null)
+		{
+			HPBar.sizeDelta = new Vector2(100f, 720f * hp);
+		}
+
+		if (Vignette != null)
+		{
+			Vignette.color = new Color(Vignette.color.r, Vignette.color.g, Vignette.color.b, hp);
+		}
+	}
+
+}
