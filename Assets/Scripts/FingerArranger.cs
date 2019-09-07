df0e4eb55b9300c74d67468b611c92e53115bdc7
Unknown <kawaxbart@gmail.com>
2019-09-07 05:57:07 +0200

    Add finger input system

---
+++ Assets/Scripts/FingerArranger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerArranger : MonoBehaviour
+{
+    string[] fingers = { "qaz", "wsx", "edc", "rfv" };
+    int[] startArrangement = { 1, 2, 3, 0 };
+    int[] currentArrangement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentArrangement = new int[startArrangement.Length];
+
+        for (int i = 0; i < currentArrangement.Length; i++)
+        {
+            currentArrangement[i] = startArrangement[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            bool isGood = false;
+
+            for (int i = 0; i < fingers.Length; i++)
+            {
+                if (currentArrangement[i] > 0 && Input.GetKeyDown(fingers[i][startArrangement[i] - currentArrangement[i]].ToString()))
+                {
+                    Debug.Log("good");
+                    --currentArrangement[i];
+                    isGood = true;
+                    break;
+                }
+            }
+
+            if (!isGood)
+                Debug.Log("bad");
+        }
+    }
+}
