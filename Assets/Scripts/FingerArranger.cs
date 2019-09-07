4f9dcc38b41a83fcf8d1326ff542831fbc756455
Unknown <kawaxbart@gmail.com>
2019-09-07 06:38:15 +0200

    Add random combo

--- Assets/Scripts/FingerArranger.cs
+++ Assets/Scripts/FingerArranger.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class FingerArranger : MonoBehaviour
 {
     string[] fingers = { "qaz", "wsx", "edc", "rfv" };
-    int[] startArrangement = { 1, 2, 3, 0 };
+    int[] startArrangement;
     int[] currentArrangement;
 
     // Start is called before the first frame update
     void Start()
     {
+        startArrangement = new int[fingers.Length];
         currentArrangement = new int[startArrangement.Length];
-
-        for (int i = 0; i < currentArrangement.Length; i++)
-        {
-            currentArrangement[i] = startArrangement[i];
-        }
+        randomNewArrangement();
     }
 
     // Update is called once per frame
@@ -38,7 +35,46 @@ public class FingerArranger : MonoBehaviour
             }
 
             if (!isGood)
+            {
                 Debug.Log("bad");
+                randomNewArrangement();
+            }
         }
+    }
+
+    private void randomNewArrangement()
+    {
+        for (int i = 0; i < startArrangement.Length; i++)
+        {
+            currentArrangement[i] = startArrangement[i] = Random.Range(0, fingers[i].Length + 1);
+        }
+
+        int zerosCounter = 0;
+
+        foreach (int x in startArrangement)
+        {
+            if (x == 0)
+                zerosCounter++;
+        }
+
+        if (zerosCounter < 1)
+        {
+            int randomFinger = Random.Range(0, fingers.Length);
+            currentArrangement[randomFinger] = startArrangement[randomFinger] = Random.Range(1, fingers[randomFinger].Length + 1);
+        }
+
+        if (zerosCounter > 3)
+        {
+            int randomFinger = Random.Range(0, fingers.Length);
+            currentArrangement[randomFinger] = startArrangement[randomFinger] = 0;
+        }
+
+        //DEBUG
+        string newArrangement = "";
+        foreach (var x in startArrangement)
+        {
+            newArrangement += x + " ";
+        }
+        Debug.LogWarning(newArrangement);
     }
 }
