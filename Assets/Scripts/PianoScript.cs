418227479f3a67d5238b63af765cb87633129178
Dirtowladny <dirtowladny@gmail.com>
2019-09-07 18:50:21 +0200

    added Piano

---
+++ Assets/Scripts/PianoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoScript : MonoBehaviour
+{
+	public AudioSource[] piano;
+	KeyCode[] keys = { KeyCode.Q, KeyCode.A,KeyCode.Z,KeyCode.W,KeyCode.S,KeyCode.X,KeyCode.E,KeyCode.D,KeyCode.C,KeyCode.R,KeyCode.F,KeyCode.V};
+
+    // Update is called once per frame
+    void Update()
+    {
+		for (int i = 0; i < keys.Length; i++)
+		{
+			if (Input.GetKeyDown(keys[i]))
+			{
+				piano[i].Play();
+			}
+		}
+    }
+}
